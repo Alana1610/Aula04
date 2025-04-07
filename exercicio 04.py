c7b69acddca73fcce006fa798851677e5be5a66e
AlanaMaria <mariaalana730@gmail.com>
2025-04-07 16:33:29 -0300

    tabuada

--- exercicio 04.py
+++ exercicio 04.py
@@ -1,5 +1,5 @@
 soma= 0
-for x in range (10):
+for x in range (1,10):
     numero = int(input("Diga seu numero:"))
     soma = soma+numero
 print("A soma foi",{soma})
---
+++ exercicio 05.py
@@ -0,0 +1,4 @@
+numero = int (input("escreva seu numero:"))
+print(f"Tabuada do {numero}:\n")
+for i in range (0,11):
+    print(f"{i} x {numero}= {i * numero}")
